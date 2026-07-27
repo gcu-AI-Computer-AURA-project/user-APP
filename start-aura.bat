aaa1a410e12ada621a9e4407fbecf95ec92f4b16
AURA <aura@example.local>
2026-07-27 15:45:05 +0900

    Prepare AURA APK build

---
+++ start-aura.bat
@@ -0,0 +1,6 @@
+@echo off
+set "PATH=C:\Program Files\nodejs;%PATH%"
+cd /d "%~dp0"
+"C:\Program Files\nodejs\npm.cmd" start
+cd C:\Users\USER\Documents\Codex\2026-07-01\figma-ux\aura-app
+.\start-aura.bat
